7e4b6f9a2e1e485d4224e53910194d1f160e0b48
André Lashley <andre.lashley@gmail.com>
2024-09-08 22:21:57 -0700

    Finish project

--- Pages/HomePage.xaml.cs
+++ Pages/HomePage.xaml.cs
@@ -1,4 +1,5 @@
 using Maui.TikTok.Models;
+using Microsoft.Maui;
 using System.Collections.ObjectModel;
 
 namespace Maui.TikTok.Pages;
@@ -15,12 +16,31 @@ public partial class HomePage : ContentPage
 
         Videos = new ObservableCollection<Video> 
         {
-            new Video { Title = "Hello World", Uri = "https://appspace.sfo3.cdn.digitaloceanspaces.com/workout1.mp4" },
-            new Video { Title = "Hello World", Uri = "https://appspace.sfo3.cdn.digitaloceanspaces.com/workout1.mp4" },
-            new Video { Title = "Hello World", Uri = "https://appspace.sfo3.cdn.digitaloceanspaces.com/workout1.mp4" },
+            new Video { Title = "Hello World", Uri = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" },
+            new Video { Title = "Hello World", Uri = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" },
+            new Video { Title = "Hello World", Uri = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" },
         };
 
         CvVideos.ItemsSource = Videos;
+    }
+
+    private void OnHeartTapped(object sender, EventArgs e)
+    {
+        var heartIcon = sender as Image;
+
+        if (heartIcon != null)
+        {
+            var currentSource = heartIcon.Source as FileImageSource;
+
+            if (currentSource != null && currentSource.File == "ic_heart_empty.png")
+            {
+                heartIcon.Source = "ic_heart_filled.png";
+            }
+            else
+            {
+                heartIcon.Source = "ic_heart_empty.png";
+            }
+        }
     }
 
     private void ItemsView_OnScrolled(object sender, ItemsViewScrolledEventArgs e)
---
+++ README.md
@@ -0,0 +1,5 @@
+# Maui.TikTok
+
+TikTok clone built with .NET Maui. 
+
+<img src="https://appspace.sfo3.cdn.digitaloceanspaces.com/maui-tiktok-screenshot.jpg" width="200"> 
